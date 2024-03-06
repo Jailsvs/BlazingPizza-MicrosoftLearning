311cc96bb007811c6d9e9d7389b0393f25c72558
Jailson Volnei dos Santos <jailsvs@gmail.com>
2024-03-06 07:35:21 -0300

    Alteração checkout para validação com EditContext ao invés de Modelo

--- Model/Address.cs
+++ Model/Address.cs
@@ -6,22 +6,22 @@ public class Address
 {
     public int Id { get; set; }
     
-    [Required, MinLength(3, ErrorMessage = "Por favor utilize um Nome maior que 3 caracteres."), MaxLength(100, ErrorMessage = "Por favor utilize um Nome menor que 100 caracteres.")]
+    [Required(ErrorMessage = "Por favor informe um Nome"), MinLength(3, ErrorMessage = "Por favor utilize um Nome maior que 3 caracteres."), MaxLength(100, ErrorMessage = "Por favor utilize um Nome menor que 100 caracteres.")]
     public string Name { get; set; }
     
-    [Required, MinLength(5, ErrorMessage = "Por favor utilize um Endereço maior que 5 caracteres."), MaxLength(100, ErrorMessage = "Por favor utilize um Endereço menor que 100 caracteres.")]
+    [Required(ErrorMessage = "Por favor informe a Linha 1"), MinLength(5, ErrorMessage = "Por favor utilize um Endereço maior que 5 caracteres."), MaxLength(100, ErrorMessage = "Por favor utilize um Endereço menor que 100 caracteres.")]
     public string Line1 { get; set; }
     
     [MaxLength(100)]
     public string Line2 { get; set; }
 
-    [Required, MinLength(3, ErrorMessage = "Por favor utilize uma Cidade maior que 3 caracteres."), MaxLength(50, ErrorMessage = "Por favor utilize uma Cidade menor que 50 caracteres.")]
+    [Required(ErrorMessage = "Por favor informe uma Cidade"), MinLength(3, ErrorMessage = "Por favor utilize uma Cidade maior que 3 caracteres."), MaxLength(50, ErrorMessage = "Por favor utilize uma Cidade menor que 50 caracteres.")]
     public string City { get; set; }
 
-    [Required, MinLength(3, ErrorMessage = "Por favor utilize uma Região maior que 3 caracteres."), MaxLength(20, ErrorMessage = "Por favor utilize uma Região menor que 20 caracteres.")]
+    [Required(ErrorMessage = "Por favor informe uma Região"), MinLength(3, ErrorMessage = "Por favor utilize uma Região maior que 3 caracteres."), MaxLength(20, ErrorMessage = "Por favor utilize uma Região menor que 20 caracteres.")]
     public string Region { get; set; }
 
-    [Required, RegularExpression(@"^([0-9]{5})$", ErrorMessage = "Por favor utilize um Código Postal válido com 5 números.")]
+    [Required(ErrorMessage = "Por favor informe um Código Postal"), RegularExpression(@"^([0-9]{5})$", ErrorMessage = "Por favor utilize um Código Postal válido com 5 números.")]
     public string PostalCode { get; set; }
 }
 
